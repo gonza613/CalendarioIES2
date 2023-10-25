45dc545266876879fd08e8499745f06adb9d377f
gonza613 <gonzagonza613@gmail.com>
2023-10-25 15:47:52 -0300

    Migracion a AndroidX
    r

---
+++ app-remote/build/generated/source/buildConfig/androidTest/debug/org/hugoandrade/calendarviewtest/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.hugoandrade.calendarviewtest.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.hugoandrade.calendarviewtest.test";
+  public static final String BUILD_TYPE = "debug";
+}
